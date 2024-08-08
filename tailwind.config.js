0555ae401f29281b8a85932d47bf48ca5e986253
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2024-08-08 17:56:25 +0600

    Remove tailwind css cdn,add tailwind Cli,Image comprihance

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        Primary: "#fc6f42",
+        Secondary: "#daaf39",
+        lightYellow: "#FFCF49",
+        purple: "#9864D9",
+        lightPurple: "#cca3ff",
+        green: "#accb46",
+        lightGreen: "#D5F178",
+        pink: "#df7f82",
+        dark: "#292321",
+        textDark: "#4D4E51",
+        lightPink: "#ffe0d3",
+        bg: "#FFF9F2",
+      },
+    },
+  },
+  plugins: [],
+};
